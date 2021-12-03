c1a8f7f92114c4b779f4ad9ddcfcb53ceacb67b4
arun.pillai <arun.pillai@samsung.com>
2021-12-03 07:21:18 +0000

    added setup_tools.py

--- testcase/array/test_array.py
+++ testcase/array/test_array.py
@@ -7,6 +7,32 @@ def test_create_check(array_management):
     pass
 
 
+def test_unmount_pos_array(mount_array):
+    try:
+        assert mount_array.cli.unmount_array()[0] == True
+    except Exception as e:
+        logger.error("Testcase failed due to {}".format(e))
+        assert 0
+
+
+def test_delete_array(array_management):
+    try:
+        assert array_management.cli.delete_array()[0] == True
+    except Exception as e:
+        logger.error("testcase failed due to {}".format(e))
+        assert 0
+
+def test_create_array_with_out_spare(scan_dev):
+    try:
+        scan_dev.cli.reset_devel()
+        assert scan_dev.cli.create_array(spare=None)[0] == True
+        scan_dev.cli.reset_devel()
+        scan_dev.cli.create_array()
+        scan_dev.cli.mount_array()
+    except Exception as e:
+        logger.error("Testcase failed due to {}".format(e))
+        assert 0
+
 def test_array_rebuild(mount_array):
     try:
         array_info = mount_array.cli.info_array()
@@ -23,25 +49,12 @@ def test_array_rebuild(mount_array):
             raise Exception("Array is not in rebuilding state")
         mount_array.target_utils.check_rebuild_status()
         assert mount_array.cli.unmount_array()[0] == True
+        mount_array.cli.delete_array()
+        mount_array.cli.reset_devel()
     except Exception as e:
         logger.error("Testcase failed due to {}".format(e))
         assert 0
 
-
-def test_unmount_pos_array(mount_array):
-    try:
-        assert mount_array.cli.unmount_array()[0] == True
-    except Exception as e:
-        logger.error("Testcase failed due to {}".format(e))
-        assert 0
-
-
-def test_delete_array(array_management):
-    try:
-        assert array_management.cli.delete_array()[0] == True
-    except Exception as e:
-        logger.error("testcase failed due to {}".format(e))
-        assert 0
 
 
 def test_create_array_2_data_disks(scan_dev):
@@ -51,9 +64,3 @@ def test_create_array_2_data_disks(scan_dev):
         logger.error("Testcase failed due to {}".format(e))
 
 
-def test_create_array_with_out_spare(scan_dev):
-    try:
-        assert scan_dev.cli.create_array(spare=None)[0] == True
-    except Exception as e:
-        logger.error("Testcase failed due to {}".format(e))
-        assert 0
