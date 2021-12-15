a676199c49302a78faad35e5e2b063502e89d0d1
arun.pillai <arun.pillai@samsung.com>
2021-12-15 08:39:29 +0000

    adding license data

--- testcase/array/test_array.py
+++ testcase/array/test_array.py
@@ -1,3 +1,29 @@
+"""
+/*
+ *   BSD LICENSE
+ *   Copyright (c) 2021 Samsung Electronics Corporation
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Samsung Electronics Corporation nor the names of
+ *       its contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWN
+"""
 import logger, pytest
 
 logger = logger.get_logger(__name__)
@@ -22,6 +48,7 @@ def test_delete_array(array_management):
         logger.error("testcase failed due to {}".format(e))
         assert 0
 
+
 def test_create_array_with_out_spare(scan_dev):
     try:
         scan_dev.cli.reset_devel()
@@ -32,6 +59,7 @@ def test_create_array_with_out_spare(scan_dev):
     except Exception as e:
         logger.error("Testcase failed due to {}".format(e))
         assert 0
+
 
 def test_array_rebuild(mount_array):
     try:
@@ -56,11 +84,8 @@ def test_array_rebuild(mount_array):
         assert 0
 
 
-
 def test_create_array_2_data_disks(scan_dev):
     try:
         assert scan_dev.cli.create_array(data="unvme-ns-0,unvme-ns-1")[0] == False
     except Exception as e:
         logger.error("Testcase failed due to {}".format(e))
-
-
