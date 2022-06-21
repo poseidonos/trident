119c3bd203b5acf8f0e8f48f3475d1ef24db3a69
Nehal Ram <nehal.ram@samsung.com>
2022-06-21 06:42:11 +0530

    [SPS-3951,SPS-3951,SPS-3951,SPS-3951,SPS-3804,SPS-3947,SPS-3946,SPS-3945,SPS-3944,SPS-3803,SPS-3943,SPS-3942,SPS-3941,SPS-3940,SPS-3802] Automated new WT tests to verify file and block FIO on all RAID types

--- testcase/system_test/array/test_wt_array_fio.py
+++ testcase/system_test/array/test_wt_array_fio.py
@@ -5,6 +5,7 @@ from pos import POS
 import logger
 import random
 import time
+import pprint
 
 logger = logger.get_logger(__name__)
 
@@ -147,4 +148,75 @@ def test_wt_array_block_file_FIO(raid_type, nr_data_drives):
     except Exception as e:
         logger.error(f"Test script failed due to {e}")
         assert pos.client.unmount_FS(mount_point) == True
+        pos.exit_handler(expected=False)
+
+
+@pytest.mark.regression
+@pytest.mark.parametrize("raid_type, nr_data_drives",
+                        [("NORAID", 1), ("RAID0", 2), ("RAID5", 3),
+                         ("RAID10", 2), ("RAID10", 4)])
+@pytest.mark.parametrize("io_type",
+                        ["block", "xfs", "ext4"])
+def test_wt_array_one_hour_FIO(raid_type, nr_data_drives, io_type):
+    logger.info(
+        " ==================== Test : test_wt_array_xfs_file_FIO ================== "
+    )
+    try:
+        array_name = "array1"
+        assert wt_test_setup_function(array_name, raid_type, nr_data_drives) == True
+
+        assert pos.cli.info_array(array_name=array_name)[0] == True
+        array_size = int(pos.cli.array_info[array_name].get("size"))
+        vol_size = f"{array_size // (1024 * 1024)}mb"          # Volume Size in MB
+        io_size =  f"{(array_size * 0.9) // (1024 * 1024)}mb"  # FIO IO size in MB
+
+        assert pos.target_utils.create_volume_multiple(array_name, 1, "pos_vol",
+                                                    size=vol_size) == True
+        assert pos.target_utils.get_subsystems_list() == True
+        assert pos.cli.list_volume(array_name=array_name)[0] == True
+        ss_list = [ss for ss in pos.target_utils.ss_temp_list if "subsystem1" in ss]
+        assert pos.target_utils.mount_volume_multiple(array_name=array_name,
+                            volume_list=pos.cli.vols, nqn_list=ss_list) == True
+
+        for ss in pos.target_utils.ss_temp_list:
+            assert pos.client.nvme_connect(ss,
+                            pos.target_utils.helper.ip_addr[0], "1158") == True
+
+        assert pos.client.nvme_list() == True
+        nvme_devs = pos.client.nvme_list_out
+
+        # Run File IO and Block IO Parallely
+        fio_cmd = f"fio --name=random_write --ioengine=libaio --rw=randwrite \
+            --iodepth=64 --direct=1 --bs=128k --time_based --runtime=3600 --size={io_size}"
+
+        if io_type == "block":
+            io_mode = True      # Set True this to Block IO
+        else:
+            io_mode = False     # Set False this to File IO
+            assert pos.client.create_File_system(nvme_devs, fs_format=io_type)
+            out, nvme_devs = pos.client.mount_FS(nvme_devs)
+            assert out == True
+
+        out, async_io = pos.client.fio_generic_runner(nvme_devs,
+                    fio_user_data=fio_cmd, IO_mode=io_mode, run_async=True)
+        assert out == True
+
+        # Wait for async FIO completions
+        while True:
+            time.sleep(120)          # Wait for 2 minutes
+            if not async_io.is_complete():
+                logger.info("Sleep for 2 minutes. FIO is running...")
+                continue
+            break
+        assert pos.client.fio_parser()
+        logger.info(pprint.pformat(pos.client.fio_par_out["write"]))
+
+        if not io_mode:
+            assert pos.client.unmount_FS(nvme_devs) == True
+        logger.info(
+            " ============================= Test ENDs ======================================"
+        )
+    except Exception as e:
+        logger.error(f"Test script failed due to {e}")
+        #assert pos.client.unmount_FS(mount_point) == True
         pos.exit_handler(expected=False)
