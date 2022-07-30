464c2e69b1505e5021a2b1d11ab16a631e27fd6f
Nehal Ram <nehal.ram@samsung.com>
2022-07-30 17:55:08 +0530

    Hetero Tests Final set

--- testcase/system_test/hetero_device/test_hetero_array_spor_npor.py
+++ testcase/system_test/hetero_device/test_hetero_array_spor_npor.py
@@ -49,13 +49,13 @@ array = [("RAID5", 3),]
 @pytest.mark.regression
 @pytest.mark.parametrize("repeat_ops", [5])
 @pytest.mark.parametrize("raid_type, num_devs", array)
-def test_hetero_multi_array_npor(raid_type, num_devs, repeat_ops):
+def test_hetero_multi_array_npor_mounted_array_repeat(raid_type, num_devs, repeat_ops):
     """
-    Create and mount two arrays of specified RAID and num of devices. Peform NPOR. 
-    Repeat this operation 5 times.
+    Create and mount two RAID5 (Default) arrays using 3 (default) number of hetero devices.
+    Peform NPOR and verify array are mounted. Unmount and delete arrays. Repeat 5 times.
     """
     logger.info(
-        " ==================== Test : test_hetero_multi_array_npor ================== "
+        " ==================== Test : test_hetero_multi_array_npor_mounted_array_repeat ================== "
     )
     try:
         assert pos.cli.reset_devel()[0] == True
@@ -97,6 +97,7 @@ def test_hetero_multi_array_npor(raid_type, num_devs, repeat_ops):
 
             assert pos.target_utils.Npor() == True
 
+            assert pos.cli.list_array()[0] == True
             for array in pos.cli.array_dict.keys():
                 assert pos.cli.info_array(array_name=array)[0] == True
                 if pos.cli.array_dict[array].lower() == "mounted":
---
+++ testcase/system_test/hetero_device/test_hetero_multi_array_telemetry.py
@@ -0,0 +1,144 @@
+import pytest
+import traceback
+
+from lib.pos import POS
+import logger
+
+logger = logger.get_logger(__name__)
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_module():
+
+    global pos, data_dict
+    pos = POS("pos_config.json")
+
+    data_dict = pos.data_dict
+    data_dict["array"]["phase"] = "false"
+    data_dict["volume"]["phase"] = "false"
+    assert pos.target_utils.pos_bring_up(data_dict=data_dict) == True
+
+    yield pos
+
+
+def teardown_function():
+    logger.info("========== TEAR DOWN AFTER TEST =========")
+    assert pos.target_utils.helper.check_system_memory() == True
+    if pos.client.ctrlr_list()[1] is not None:
+        assert pos.client.nvme_disconnect(pos.target_utils.ss_temp_list) == True
+
+    assert pos.cli.list_array()[0] == True
+    array_list = list(pos.cli.array_dict.keys())
+    if len(array_list) == 0:
+        logger.info("No array found in the config")
+    else:
+        for array in array_list:
+            assert pos.cli.info_array(array_name=array)[0] == True
+            if pos.cli.array_dict[array].lower() == "mounted":
+                assert pos.cli.unmount_array(array_name=array)[0] == True
+
+    logger.info("==========================================")
+
+
+def teardown_module():
+    logger.info("========= TEAR DOWN AFTER SESSION ========")
+    pos.exit_handler(expected=True)
+
+
+array = [("RAID5", 3)]
+
+@pytest.mark.regression
+@pytest.mark.parametrize("array_raid, num_devs", array)
+def test_hetero_multi_array_telemetry(array_raid, num_devs):
+    """
+    Test to create two RAID5 (Default) arrays with 3 (Default) hetero devices.
+    Create and mount 100 volumes from each array. Start Telemetry. 
+    Run Block IO for 5 minutes. Stop Telemery.
+    """
+    logger.info(
+        " ==================== Test : test_hetero_multi_array_telemetry ================== "
+    )
+    try:
+        assert pos.cli.reset_devel()[0] == True
+
+        repeat_ops=5
+        num_array = 2
+        num_vols = 100
+        assert pos.target_utils.get_subsystems_list() == True
+        ss_list = pos.target_utils.ss_temp_list[:num_array]
+        for counter in range(repeat_ops):
+            logger.info(f"Start of {counter+1}/{repeat_ops} Execution!")
+            for id in range(num_array):
+                assert pos.cli.scan_device()[0] == True
+                assert pos.cli.list_device()[0] == True
+
+                # Verify the minimum disk requirement
+                if len(pos.cli.system_disks) < (num_array - id) * num_devs:
+                    pytest.skip(f"Insufficient disk count {len(pos.cli.system_disks)}. "\
+                                f"Required minimum {(num_array - id) * num_devs}")
+
+                array_name = f"array{id+1}"
+                raid_type = array_raid
+                uram_name = data_dict["device"]["uram"][id]["uram_name"]
+
+                if raid_type.lower() == "raid0" and num_devs == 2:
+                    data_device_conf = {'mix': 2}
+                else:
+                    data_device_conf = {'mix': 2, 'any': num_devs - 2}
+
+                if not pos.target_utils.get_hetero_device(data_device_conf):
+                    logger.info("Failed to get the required hetero devcies")
+                    pytest.skip("Required condition not met. Refer to logs for more details")
+
+                data_drives = pos.target_utils.data_drives
+                spare_drives = pos.target_utils.spare_drives
+
+                assert pos.cli.create_array(write_buffer=uram_name, data=data_drives, 
+                                            spare=spare_drives, raid_type=raid_type,
+                                            array_name=array_name)[0] == True
+
+                assert pos.cli.mount_array(array_name=array_name)[0] == True
+                assert pos.cli.info_array(array_name=array_name)[0] == True 
+
+                array_size = int(pos.cli.array_info[array_name].get("size"))
+                vol_size = f"{int((array_size / num_vols) / (1024 * 1024))}mb"
+                vol_name = "pos_vol"
+
+                assert pos.target_utils.create_volume_multiple(array_name, num_vols,
+                        vol_name=vol_name, size=vol_size, maxiops=0, bw=0) == True
+
+                nqn=ss_list[id]
+                assert pos.target_utils.mount_volume_multiple(array_name=array_name,
+                                volume_list=pos.cli.vols, nqn_list=[nqn]) == True
+
+                # Connect client
+                assert pos.client.nvme_connect(nqn, 
+                        pos.target_utils.helper.ip_addr[0], "1158") == True
+
+            assert pos.cli.start_telemetry()[0] == True
+
+            assert pos.client.nvme_list() == True
+            run_time = 5  # FIO for 5 minutes
+
+            # Block IO
+            fio_cmd = f"fio --name=seq_write --ioengine=libaio --rw=write "\
+                      f"--bs=128k --iodepth=64 --time_based --runtime={run_time}"
+
+            io_mode = False  # Set False for Block IO
+            assert pos.client.fio_generic_runner(pos.client.nvme_list_out,
+                                    fio_user_data=fio_cmd, IO_mode=io_mode)
+
+            assert pos.cli.stop_telemetry()[0] == True
+
+            assert pos.cli.list_array()[0] == True
+            for array in pos.cli.array_dict.keys():
+                    assert pos.cli.delete_array(array_name=array)[0] == True
+
+            logger.info(f"End of {counter+1}/{repeat_ops} Execution!")
+
+    except Exception as e:
+        logger.error(f"Test script failed due to {e}")
+        traceback.print_exc()
+
+    logger.info(
+        " ============================= Test ENDs ======================================"
+    )
