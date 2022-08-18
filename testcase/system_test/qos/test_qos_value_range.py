a7a221dcc65928cecb0b0ab668f3b2d431aafe74
Nehal Ram <nehal.ram@samsung.com>
2022-08-18 16:02:35 +0530

    [SPS-1897,SPS-1896,SPS-1901,SPS-1902,SPS-1895,SPS-1900,SPS-1899,SPS-1894,SPS-1898,SPS-1893] Added qos test for volume range

---
+++ testcase/system_test/qos/test_qos_value_range.py
@@ -0,0 +1,104 @@
+from time import time
+import traceback
+import pytest
+
+from pos import POS
+import logger
+
+logger = logger.get_logger(__name__)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_module():
+
+    global pos, data_dict, array_name, vol_name
+    pos = POS("pos_config.json")
+    data_dict = pos.data_dict
+
+    data_dict["subsystem"]["pos_subsystems"][0]["nr_subsystems"] = 1
+    data_dict["subsystem"]["pos_subsystems"][1]["nr_subsystems"] = 0
+
+    data_dict["array"]["num_array"] = 1
+    data_dict["volume"]["phase"] = "false"
+
+    array_name = data_dict["array"]["pos_array"][0]["array_name"]
+    vol_name = "POS_Vol"
+
+    assert pos.target_utils.pos_bring_up(data_dict=data_dict) == True
+    yield pos
+
+
+def teardown_function():
+    logger.info("========== TEAR DOWN AFTER TEST =========")
+
+    assert pos.cli.list_volume(array_name=array_name)[0] == True
+    for vol_name in pos.cli.vols:
+        assert pos.cli.reset_volume_policy_qos(vol_name, array_name)[0] == True
+        assert pos.cli.delete_volume(vol_name, array_name)[0] == True
+            
+    logger.info("==========================================")
+
+
+def teardown_module():
+    logger.info("========= TEAR DOWN AFTER SESSION ========")
+    pos.exit_handler(expected=True)
+
+# List of maxiops and maxbw
+
+qos_tests = {}
+# negative and decimals values for bandwidth
+qos_tests["t0"] = {"iops_bw": [(0, -1.0), (0, 5.5), (0, 15.5)],   "result":False}  
+# bandwidth values-1=9 and any iops  
+qos_tests["t1"] = {"iops_bw": [(0, 9), (10, 9), (100, 9)],      "result":False}
+# bandwidth=2^64-1 and iops 0, 10 
+qos_tests["t2"] = {"iops_bw": [(0, 2^64-1), (10, 2^64-1)],      "result":True }
+# bandwidth value=10 and any iops values       
+qos_tests["t3"] = {"iops_bw": [(0, 10), (10, 10), (100, 10)],   "result":True }
+# 0 iops and any maxbw values
+qos_tests["t4"] = {"iops_bw": [(0, 15), (0, 100), (0, 1024)],   "result":True}  
+# negative and decimals values for iops
+qos_tests["t5"] = {"iops_bw": [(-1.0, 0), (5.5, 0), (15.5, 10)],  "result":False}
+# maxiops values-1=9 and maxbw values 
+qos_tests["t6"] = {"iops_bw": [(9, 0), (9, 10), (9, 1024)],     "result":False}
+# maxiops values=2^64-1  and maxbw 10     
+qos_tests["t7"] = {"iops_bw": [(2^64-1, 0), (2^64-1, 10)],      "result":True }  
+ # maxiops values=10  and maxbw values     
+qos_tests["t8"] = {"iops_bw": [(10, 0), (10, 10), (10, 1024)],  "result":True }
+# maxiops and maxbw values using
+qos_tests["t9"] = {"iops_bw": [(15, 15), (1024, 1024), (100, 100)], "result":True}   
+
+qos_test_list = ["t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7", "t8", "t9"]
+
+@pytest.mark.regression
+@pytest.mark.parametrize("qos_test", qos_test_list)
+def test_qos_maxiops_maxbw_value(qos_test):
+    logger.info(
+        " ==================== Test : test_qos_maxiops_maxbw_value ================== "
+    )
+    try:
+        qos_values = qos_tests[qos_test]["iops_bw"]
+        exp_result = qos_tests[qos_test]["result"]
+        logger.info(f"{qos_values}, {exp_result}")
+        assert pos.cli.create_volume(vol_name, '10GB', array_name)[0] == True
+
+        for max_iops,max_bw in qos_values:
+            assert pos.cli.create_volume_policy_qos(
+                vol_name, array_name, max_iops, max_bw)[0] == exp_result 
+            
+            if exp_result:
+                assert pos.cli.info_volume(
+                    array_name=array_name, vol_name=vol_name)[0] == True
+
+                vol_info = pos.cli.volume_info[array_name][vol_name]
+                assert vol_info["max_iops"] == max_iops
+                assert vol_info["max_bw"] == max_bw
+
+        logger.info(
+            " ============================= Test ENDs ======================================"
+        )
+    except Exception as e:
+        logger.error(f"Test script failed due to {e}")
+        traceback.print_exc()
+        assert 0
+
+        
