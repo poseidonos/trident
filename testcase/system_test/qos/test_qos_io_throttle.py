982d69ebe1413c3c884aa90240f139c1486bf69b
Nehal Ram <nehal.ram@samsung.com>
2022-08-23 16:07:20 +0530

    [SPS-1219,SPS-1174,SPS-911,SPS-1111,SPS-946,SPS-1064,SPS-1392,SPS-1259,SPS-975,SPS-1357,SPS-1424,SPS-958] Automated qos io throtteling tests

--- testcase/system_test/qos/test_qos_io_throttle.py
+++ testcase/system_test/qos/test_qos_io_throttle.py
@@ -47,23 +47,19 @@ def teardown_module():
 
 
 qos_iops_bw = [
-    (15,    5000), (5000,   100),  (5,	10),
-    (12, 50), (15.0,    100.0), (5000,	10),
+    (15,    5000), (5000,   100),  (50, 10),
+    (12, 50), (15.0,    100.0), (5000,  10),
 ]
 fio_io_type = ["file", "block"]
 
-
 @pytest.mark.regression
-@pytest.mark.parametrize("iops_bw", qos_iops_bw)
+@pytest.mark.parametrize("max_iops, max_bw", qos_iops_bw)
 @pytest.mark.parametrize("io_type", fio_io_type)
-def test_qos_io_throttle(iops_bw, io_type):
+def test_qos_io_throttle(max_iops, max_bw, io_type):
     logger.info(
-        f" ========== Test : test_qos_io_throttle[{iops_bw}-{io_type}] ============ "
+        f" ========== Test : test_qos_io_throttle[{max_iops}-{max_bw}-{io_type}] ============ "
     )
     try:
-        max_iops = iops_bw[0]
-        max_bw = iops_bw[1]
-
         assert pos.cli.create_volume(vol_name, '12GB', array_name)[0] == True
 
         assert pos.cli.create_volume_policy_qos(
@@ -84,8 +80,8 @@ def test_qos_io_throttle(iops_bw, io_type):
         assert pos.client.nvme_list() == True
         nvme_devs = pos.client.nvme_list_out
 
-        fio_cmd = f"fio --name=sequential_write --ioengine=libaio --rw=write \
-                            --iodepth=64 --direct=1 --bs=128k --size=10g"
+        fio_cmd = f"fio --name=sequential_write --ioengine=libaio --rw=write "\
+                    "--iodepth=64 --direct=1 --bs=128k --size=1g"
 
         if fio_io_type == "file":   # Run File IO
             assert pos.client.create_File_system(
@@ -94,16 +90,20 @@ def test_qos_io_throttle(iops_bw, io_type):
             assert out == True
             io_mode = False  # Set False this to File IO
             assert pos.client.fio_generic_runner(
-                mount_point, fio_user_data=fio_cmd, IO_mode=io_mode) == True
+                mount_point, fio_user_data=fio_cmd, IO_mode=io_mode)[0] == True
             assert pos.client.unmount_FS(mount_point) == True
         else:   # Run Block IO
             io_mode = True  # Set True for Block IO
             assert pos.client.fio_generic_runner(
-                nvme_devs, fio_user_data=fio_cmd, IO_mode=io_mode) == True
+                nvme_devs, fio_user_data=fio_cmd, IO_mode=io_mode)[0] == True
+
+        fio_out = {}
+        fio_out["iops"] = pos.client.fio_par_out["write"]["iops"]
+        fio_out["bw"] = pos.client.fio_par_out["write"]["bw"] / 1024  # Conver to MB
 
         # Verify the QOS Throttling
-        assert pos.client.fio_verify_qos({max_iops, max_bw},
-                                         pos.client.fio_par_out["write"],
+        assert pos.client.fio_verify_qos({"max_iops":max_iops, "max_bw":max_bw},
+                                         fio_out,
                                          len(nvme_devs)) == True
 
         logger.info(
