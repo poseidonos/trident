090910826b28b391eb664f811c34c53564b33897
U-CORP\muhammad.m <muhammad.m@muhammad-m02.sa.corp.samsungelectronics.net>
2022-08-27 20:34:34 +0530

    [SPS-2881,SPS-2874,SPS-2841,SPS-2499,SPS-2463,SPS-2214,SPS-2197,SPS-2194,SPS-2192,SPS-2164,SPS-2160,SPS-2140,SPS-2137,SPS-2136,SPS-2133,SPS-2019,SPS-1919,SPS-1918,SPS-1915,SPS-1914,SPS-1913,SPS-1911,SPS-1910,SPS-1908,SPS-1815,SPS-1802,SPS-1801,SPS-1770,SPS-1748,SPS-1722,SPS-760,SPS-692,SPS-682,SPS-674,SPS-394,SPS-392]-adding new cases to array-volumes-states-negative-scenarios

--- testcase/system_test/array/test_negative_arrayo.py
+++ testcase/system_test/array/test_negative_arrayo.py
@@ -158,6 +158,31 @@ def test_add_spare_without_array_mount(raid_type="RAID5", nr_data_drives=3):
         pos.exit_handler(expected=False)
 
 @pytest.mark.regression
+def test_remove_spare_without_array_mount(raid_type="RAID5", nr_data_drives=3):
+    logger.info(" ==================== Test : test_add_spare_without_mount ================== ")
+    try:
+        assert negative_test_setup_function(nr_data_drives) == True
+        spare_disk_list = [system_disks.pop()]
+        assert (
+            pos.cli.create_array(
+                write_buffer="uram0",
+                data=data_disk_list,
+                spare=spare_disk_list,
+                raid_type=raid_type,
+                array_name=array_name,
+            )[0]
+            == True
+        )
+        assert pos.cli.rmspare_array(array_name=array_name,device_name=spare_disk_list[0])[0] == False
+        logger.info(
+            " ============================= Test ENDs ======================================"
+        )
+    except Exception as e:
+        logger.error(f"Test script failed due to {e}")
+        pos.exit_handler(expected=False)
+
+
+@pytest.mark.regression
 def test_array_mnt_without_arrays(nr_data_drives=3):
     logger.info(" ==================== Test : test_array_cli_wt ================== ")
     try:
@@ -171,6 +196,7 @@ def test_array_mnt_without_arrays(nr_data_drives=3):
     except Exception as e:
         logger.error(f"Test script failed due to {e}")
         pos.exit_handler(expected=False)
+
 
 @pytest.mark.regression
 def test_mnt_vol_stop_arrray_state(raid_type="RAID5", nr_data_drives=3):
@@ -245,13 +271,28 @@ def test_rename_vol_stop_arrray_state(raid_type="RAID5", nr_data_drives=3):
         logger.error(f"Test script failed due to {e}")
         pos.exit_handler(expected=False)
 
+
+
 @pytest.mark.regression
-def test_array_state_vol_state(raid_type="RAID5", nr_data_drives=3):
-    logger.info(
-        " ==================== Test : test_mnt_vol_fault_arrray_state ================== "
-    )
+def test_uram_creation_after_scan(nr_data_drives=3):
+    logger.info(" ==================== Test : test_uram_creation_after_scan ================== ")
     try:
         assert negative_test_setup_function(nr_data_drives) == True
+        assert pos.cli.scan_device()[0] == True
+        assert pos.cli.create_device(uram_name='uram2',bufer_size=8388608,strip_size=512,numa=0)[0] == True
+        logger.info(
+            " ============================= Test ENDs ======================================"
+        )
+    except Exception as e:
+        logger.error(f"Test script failed due to {e}")
+        pos.exit_handler(expected=False)
+
+@pytest.mark.regression
+def test_delete_create_array(raid_type="RAID5", nr_data_drives=3):
+    logger.info(" ==================== Test : test_add_spare_without_mount ================== ")
+    try:
+        assert negative_test_setup_function(nr_data_drives) == True
+        spare_disk_list = [system_disks.pop()]
         assert (
             pos.cli.create_array(
                 write_buffer="uram0",
@@ -262,21 +303,46 @@ def test_array_state_vol_state(raid_type="RAID5", nr_data_drives=3):
             )[0]
             == True
         )
-        assert pos.cli.mount_array(array_name=array_name, write_back=True)[0] == True
-        assert pos.cli.info_array(array_name)[0] == True
-        array_status = pos.cli.array_info[array_name]
-        logger.info(str(array_status))
-        logger.info(array_status["state"])
-        if pos.cli.array_info[array_name]["state"] == "NORMAL":
-            assert pos.cli.create_volume(array_name=array_name, size="10gb", volumename="vol")[0]== True
-            assert pos.cli.list_volume(array_name=array_name)[0] == True
-            assert pos.cli.mount_volume(array_name=array_name,volumename=pos.cli.vols[0])[0]== True
-            assert pos.cli.unmount_volume(array_name=array_name,volumename=pos.cli.vols[0])[0]== True
-            logger.info("Expected array state mismatch with output{} and volume mount was sucessful".format(array_status["state"]))
-        else:
-            assert 0
+        assert pos.target_utils.device_hot_remove(system_disks[:1]) == True
+        assert pos.cli.delete_array(array_name=array_name)
+        assert (
+            pos.cli.create_array(
+                write_buffer="uram0",
+                data=data_disk_list,
+                spare=None,
+                raid_type=raid_type,
+                array_name=array_name,
+            )[0]
+            == True
+        )
+        logger.info(
+            " ============================= Test ENDs ======================================"
+        )
     except Exception as e:
         logger.error(f"Test script failed due to {e}")
         pos.exit_handler(expected=False)
 
+@pytest.mark.regression
+def test_logger_info(raid_type="RAID5", nr_data_drives=3):
+    logger.info(" ==================== Test : test_add_spare_without_mount ================== ")
+    try:
+        assert negative_test_setup_function(nr_data_drives) == True
+        spare_disk_list = [system_disks.pop()]
+        assert (
+            pos.cli.create_array(
+                write_buffer="uram0",
+                data=data_disk_list,
+                spare=None,
+                raid_type=raid_type,
+                array_name=array_name,
+            )[0]
+            == True
+        )
+        assert pos.cli.info_logger()[0] == True
+        logger.info(
+            " ============================= Test ENDs ======================================"
+        )
+    except Exception as e:
+        logger.error(f"Test script failed due to {e}")
+        pos.exit_handler(expected=False)
 
