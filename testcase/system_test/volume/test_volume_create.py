9450f9d6a8b398ad1749a981931a7df065642f27
Sohan Singh <sohan.singh@samsung.com>
2022-08-29 20:38:41 +0530

    [SPS-2838,SPS-678,SPS-679,SPS-641] SPS-2838 : Unmount Volume SPS-678 : Unmount the Connected Volume from target SPS-679 :  Mount non existing volume SPS-641 : Create and delete volume with loop and run IO on active Volumes.

--- testcase/system_test/volume/test_volume_create.py
+++ testcase/system_test/volume/test_volume_create.py
@@ -2,6 +2,7 @@ import pytest
 import logger
 import random
 import string
+import time
 
 from pos import POS
 
@@ -280,3 +281,194 @@ def test_volume_create_without_array_mount():
         logger.info(f" Test Script failed due to {e}")
         pos.exit_handler(expected=False)
         assert 0
+
+
+@pytest.mark.regression
+def test_multiple_volume_create_delete_with_io():
+    '''The purpose of test is to create and delete volume in loop.
+        Run IO on the active volumes with data integrity check.'
+        '''
+    logger.info(
+        "================ Test : test_multiple_volume_create_delete_with_io =================")
+    try:
+        pos.target_utils.get_subsystems_list()
+        assert pos.target_utils.create_volume_multiple(
+            array_name=array_name, num_vol=15, size="1gb"
+        ) == True
+
+        assert pos.cli.list_volume(array_name=array_name)[0] == True
+        volume_list = pos.cli.vols
+
+        assert pos.target_utils.mount_volume_multiple(
+            array_name=array_name,
+            volume_list=volume_list,
+            nqn_list=[pos.target_utils.ss_temp_list[0]]
+        ) == True
+
+        for _ in range(12):
+            assert pos.cli.unmount_volume(
+                array_name=array_name, volumename=volume_list[_])[0] == True
+            assert pos.cli.delete_volume(
+                array_name=array_name, volumename=volume_list[_])[0] == True
+
+        assert pos.target_utils.create_volume_multiple(
+            array_name=array_name, num_vol=3, size="1gb"
+        ) == True
+
+        assert pos.cli.list_volume(array_name=array_name)[0] == True
+        volume_list = pos.cli.vols
+        for vol in volume_list:
+            assert pos.cli.info_volume(array_name=array_name, vol_name=vol)
+            if pos.cli.volume_info[array_name][vol]["status"] == "Unmounted":
+                assert pos.cli.mount_volume(
+                    array_name=array_name, volumename=vol, nqn=pos.target_utils.ss_temp_list[0])[0] == True
+
+        assert pos.client.nvme_connect(pos.target_utils.ss_temp_list[0],
+                                       pos.target_utils.helper.ip_addr[0], "1158") == True
+
+        assert pos.client.nvme_list() == True
+        pos.client.check_system_memory()
+
+        nvme_devs = pos.client.nvme_list_out
+
+        fio_cmd = f"fio --name=sequential_write --runtime=300 --ramp_time=60  --ioengine=libaio  --iodepth=16 --rw=write --size=5g --bs=4kb --direct=1 --offset=0 --verify=crc32c"
+
+        block_io_devs = nvme_devs
+        io_mode = True  # Block IO
+        out, async_block_io = pos.client.fio_generic_runner(
+            block_io_devs, fio_user_data=fio_cmd, IO_mode=io_mode, run_async=True
+        )
+        assert out == True
+
+        # Wait for async FIO completions
+        while True:
+            time.sleep(30)  # Wait for 30 seconds
+            block_io = async_block_io.is_complete()
+
+            msg = []
+            if not block_io:
+                msg.append("Block IO")
+
+            if msg:
+                logger.info(
+                    "'{}' is still running. Wait 30 seconds...".format(
+                        ",".join(msg))
+                )
+                continue
+            break
+
+        pos.client.check_system_memory()
+
+        logger.info("=============== TEST ENDs ================")
+
+    except Exception as e:
+        logger.info(f" Test Script failed due to {e}")
+        pos.exit_handler(expected=False)
+
+
+@pytest.mark.regression
+def test_unmount_volume():
+    '''The purpose of test is to unmount the volume from target'''
+
+    logger.info(
+        "================ Test : test_unmount_volume =================")
+    try:
+        assert pos.cli.scan_device()[0] == True
+        assert pos.cli.list_device()[0] == True
+        system_disks = pos.cli.system_disks
+        data_disk_list = [system_disks.pop(0) for i in range(nr_data_drives)]
+
+        assert pos.cli.create_array(write_buffer="uram1",
+                                    data=data_disk_list,
+                                    spare=None,
+                                    raid_type="RAID5",
+                                    array_name="POS_" + array_name,
+                                    )[0] == True
+
+        assert pos.cli.mount_array(
+            array_name="POS_" + array_name,
+        )[0] == True
+
+        assert pos.cli.list_array()[0] == True
+        assert pos.cli.create_volume(
+            volumename="vol_1", size="10gb", array_name="POS_" + array_name)[0] == True
+        assert pos.cli.info_volume(array_name="POS_" + array_name, vol_name="vol_1")[
+            0] == True
+
+        assert pos.cli.mount_volume(
+            array_name="POS_" + array_name, volumename="vol_1")[0] == True
+        assert pos.cli.list_subsystem()[0] == True
+
+        assert pos.cli.unmount_volume(
+            array_name="POS_" + array_name, volumename="vol_1")[0] == True
+
+        assert pos.cli.list_subsystem()[0] == True
+
+        # Unmounting & Deleting the Array
+        assert pos.cli.unmount_array(
+            array_name="POS_" + array_name)[0] == True
+        assert pos.cli.delete_array(
+            array_name="POS_" + array_name)[0] == True
+
+        logger.info("=============== TEST ENDs ================")
+
+    except Exception as e:
+        logger.info(f" Test Script failed due to {e}")
+        pos.exit_handler(expected=False)
+
+
+@pytest.mark.regression
+def test_unmount_volume_connected():
+    '''The purpose of test is to unmount the connected volume from target'''
+
+    logger.info(
+        "================ Test : test_unmount_volume_connected =================")
+
+    try:
+        assert pos.cli.create_volume(
+            array_name=array_name, size="10gb", volumename="vol1"
+        )[0] == True
+
+        assert pos.cli.info_volume(
+            array_name=array_name, vol_name="vol1")[0] == True
+
+        pos.target_utils.get_subsystems_list()
+        assert pos.cli.list_subsystem()[0] == True
+
+        assert pos.cli.mount_volume(
+            array_name=array_name, volumename="vol1", nqn=pos.target_utils.ss_temp_list[0])[0] == True
+
+        assert pos.client.nvme_connect(pos.target_utils.ss_temp_list[0],
+                                       pos.target_utils.helper.ip_addr[0], "1158") == True
+
+        assert pos.client.nvme_list() == True
+        # Unmounting the connected Volume
+        assert pos.cli.unmount_volume(
+            array_name=array_name, volumename="vol1")[0] == False
+        assert pos.client.nvme_disconnect(
+            nqn=[pos.target_utils.ss_temp_list[0]]) == True
+        assert pos.cli.unmount_volume(
+            array_name=array_name, volumename="vol1")[0] == True
+
+        logger.info("=============== TEST ENDs ================")
+
+    except Exception as e:
+        logger.info(f" Test Script failed due to {e}")
+        pos.exit_handler(expected=False)
+
+
+@pytest.mark.regression
+def test_volume_mount_invalid_name():
+    '''The purpose of test is to mount a volume with non-existing volume name'''
+
+    logger.info(
+        "================ Test : test_volume_mount_invalid_name =================")
+
+    try:
+
+        assert pos.cli.mount_volume(
+            array_name=array_name, volumename="non_existing_vol")[0] == False
+
+    except Exception as e:
+        logger.info(f"Test script failed due to {e}")
+        pos.exit_handler(expected=False)
