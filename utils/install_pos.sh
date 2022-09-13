c7ac02f9be94423f604deab6b0880416d8a2f2bc
Sohan Singh <sohan.singh@samsung.com>
2022-09-13 16:12:32 +0530

    code for Bamboo integration

---
+++ utils/install_pos.sh
@@ -0,0 +1,20 @@
+branchName='v0.10.0'
+export GIT_SSL_NO_VERIFY=1
+echo `date +%Y-%m-%d` `date +%H:%M:%S`
+mkdir -p /root/poseidon
+cd /root/poseidon
+echo Remove ibofos directory
+rm -rf ibofos
+git clone https://git.memswdev.samsungds.net:7990/scm/ibof/ibofos.git
+cd ibofos
+git checkout $branchName
+cd script
+./pkgdep.sh | tee pkgdep.log
+cd ../lib/spdk/scripts
+./pkgdep.sh | tee pkgdep.log
+cd /root/poseidon/pos/lib
+./build_lib_internal.sh | tee build_lib_internal.log
+cd ../script
+./build_ibofos.sh -i | tee build_ibofos.log
+cd script
+./setup_env.sh
