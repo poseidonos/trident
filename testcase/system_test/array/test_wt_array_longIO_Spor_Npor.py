38b9193891f4e9950323b170ecc688701d0aa331
arun.pillai <arun.pillai@samsung.com>
2022-09-20 13:56:13 +0530

    updated as per SAM score

--- testcase/system_test/array/test_wt_array_longIO_Spor_Npor.py
+++ testcase/system_test/array/test_wt_array_longIO_Spor_Npor.py
@@ -1,5 +1,5 @@
 import pytest
-from array_test_common import *
+from common_libs import *
 
 import logger
 logger = logger.get_logger(__name__)
@@ -18,69 +18,33 @@ logger = logger.get_logger(__name__)
         ("RAID10",8,"Npor"),
     ],
 )
-def test_wb_wt_array_long_fileIO_Npor_Spor(array_setup_cleanup, raid_type, nr_data_drives, por):
+def test_wb_wt_array_long_fileIO_Npor_Spor(array_fixture, raid_type, nr_data_drives, por):
 
     logger.info(
         " ==================== Test : test_wb_wt_array_long_fileIO_Npor_Spor ================== "
     )
     try:
-        pos = array_setup_cleanup
-        assert wt_array_setup(pos, raid_type, nr_data_drives, 0) == True
-        array_name = pos.data_dict['array']['pos_array'][0]["array_name"]
-
-        if pos.target_utils.helper.check_pos_exit() == True:
-            assert pos.target_utils.pos_bring_up(data_dict=pos.data_dict) == True
-        assert pos.cli.reset_devel()[0] == True
-
-        assert pos.cli.scan_device()[0] == True
-        assert pos.cli.list_device()[0] == True
-        system_disks = pos.cli.system_disks
-        if len(system_disks) < (nr_data_drives + 1):
-            pytest.skip(
-                f"Insufficient disk count {system_disks}. Required minimum {nr_data_drives + 1}"
-            )
-        data_disk_list = [system_disks.pop(0) for i in range(nr_data_drives)]
-        array_name = "posarray1"
-        assert (
-            pos.cli.create_array(
-                write_buffer="uram0",
-                data=data_disk_list,
-                spare=None,
-                raid_type=raid_type,
-                array_name=array_name,
-            )[0]
-            == True
-        )
-
-        assert pos.cli.mount_array(array_name=array_name)[0] == True
-        assert (
-            pos.cli.create_volume("pos_vol_1", array_name=array_name, size="2000gb")[0]
-            == True
-        )
-        assert pos.target_utils.get_subsystems_list() == True
-        assert pos.cli.list_volume(array_name=array_name)[0] == True
-        ss_list = [ss for ss in pos.target_utils.ss_temp_list if "subsystem1" in ss]
-        assert (
-            pos.target_utils.mount_volume_multiple(
-                array_name=array_name, volume_list=pos.cli.vols, nqn_list=ss_list
-            )
-            == True
-        )
-
-        assert (
-            pos.client.nvme_connect(
-                ss_list[0], pos.target_utils.helper.ip_addr[0], "1158"
-            )
-            == True
-        )
-        assert pos.client.nvme_list() == True
+        pos = array_fixture
+        pos.data_dict['array']['pos_array'][0]['raid_type'] = raid_type
+        pos.data_dict['array']['pos_array'][1]['raid_type'] = raid_type
+        pos.data_dict['array']['pos_array'][0]['data_device'] = nr_data_drives
+        pos.data_dict['array']['pos_array'][1]['data_device'] = nr_data_drives
+        pos.data_dict['volume']['pos_volumes'][0]['num_vol'] = 1
+        pos.data_dict['volume']['pos_volumes'][1]['num_vol'] = 1
+        pos.data_dict['volume']["pos_volumes"][0]["size"] = "200gb"
+        
+        logger.info("configuring POS as per TC req")
+        assert pos.target_utils.bringupArray(data_dict = pos.data_dict) == True
+        assert pos.target_utils.bringupVolume(data_dict = pos.data_dict) == True
+        run_io(pos)
+        
         dev = [pos.client.nvme_list_out[0]]
 
         assert pos.client.create_File_system(dev, fs_format="xfs")
         status, mount_point = pos.client.mount_FS(dev)
         assert status == True
 
-        fio_cmd = "fio --name=Rand_RW  --runtime=42000 --ramp_time=60  --ioengine=sync  --iodepth=32 --rw=write --size=1000g bs=32kb --direct=1 --verify=md5"
+        fio_cmd = "fio --name=Rand_RW  --runtime=100 --ramp_time=60  --ioengine=sync  --iodepth=32 --rw=write --size=50g --bs=32kb --direct=1 --verify=md5"
 
         status, io_pro = pos.client.fio_generic_runner(
             mount_point, fio_user_data=fio_cmd, IO_mode=False, run_async=True
@@ -94,66 +58,7 @@ def test_wb_wt_array_long_fileIO_Npor_Spor(array_setup_cleanup, raid_type, nr_da
         else:
             assert pos.target_utils.Spor(uram_backup=True) == True
 
-        # unmount and delete array and volume
-
-        assert pos.cli.list_volume(array_name=array_name)[0] == True
-        for volume in pos.cli.vols:
-            pos.cli.unmount_volume(volumename=volume, array_name=array_name)
-            pos.cli.delete_volume(volumename=volume, array_name=array_name)
-        assert pos.cli.info_array(array_name=array_name)[0] == True
-        assert pos.cli.unmount_array(array_name=array_name)[0] == True
-        assert pos.cli.delete_array(array_name=array_name)[0] == True
-
-        array_name = "posarray2"
-        assert (
-            pos.cli.create_array(
-                write_buffer="uram0",
-                data=data_disk_list,
-                spare=None,
-                raid_type=raid_type,
-                array_name=array_name,
-            )[0]
-            == True
-        )
-
-        assert pos.cli.mount_array(array_name=array_name, write_back=False)[0] == True
-        assert (
-            pos.cli.create_volume("pos_vol_1New", array_name=array_name, size="2000gb")[
-                0
-            ]
-            == True
-        )
-        assert pos.target_utils.get_subsystems_list() == True
-        assert pos.cli.list_volume(array_name=array_name)[0] == True
-        ss_list = [ss for ss in pos.target_utils.ss_temp_list if "subsystem2" in ss]
-        assert (
-            pos.target_utils.mount_volume_multiple(
-                array_name=array_name, volume_list=pos.cli.vols, nqn_list=ss_list
-            )
-            == True
-        )
-
-        assert (
-            pos.client.nvme_connect(
-                ss_list[0], pos.target_utils.helper.ip_addr[0], "1158"
-            )
-            == True
-        )
-        assert pos.client.nvme_list() == True
-        dev = [pos.client.nvme_list_out[0]]
-        assert pos.client.create_File_system(dev, fs_format="xfs") == True
-        status, mount_point_new = pos.client.mount_FS(dev)
-        assert status == True
-
-        fio_cmd = "fio --name=Rand_RW  --runtime=43000 --ramp_time=60 --ioengine=sync  --iodepth=32 --rw=write --size=10g bs=32kb --direct=1 --verify=md5"
-
-        status, io_pro = pos.client.fio_generic_runner(
-            mount_point_new, fio_user_data=fio_cmd, IO_mode=False, run_async=True
-        )
-        assert status == True
-        assert pos.client.unmount_FS(mount_point_new) == True
-        assert pos.client.delete_FS(mount_point_new) == True
-
+        
         logger.info(
             " ============================= Test ENDs ======================================"
         )
