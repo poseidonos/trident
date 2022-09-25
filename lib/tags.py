c35a72b5a2d32890d29577f63108104cdbe0990b
arun.pillai <arun.pillai@samsung.com>
2022-09-25 12:05:12 +0530

    added threads in nvme_disconnect() for faster execution

--- lib/tags.py
+++ lib/tags.py
@@ -36,11 +36,7 @@ class EnvTags(SSHclient):
     @threaded
     def get_tags(self):
         try:
-            logger.info(
-                "Running command 'lshw -xml -numeric' in the node : {}".format(
-                    self.item[0]
-                )
-            )
+            
             inventory = self.conn.execute("lshw -xml -numeric")
             inventory = "\n".join(inventory)
             inventory = etree.XML(inventory)
@@ -143,17 +139,15 @@ class EnvTags(SSHclient):
                         + net.find("vendor").text
                     )
 
-            logger.info(
-                "Runnning command 'hostname' Static hostname in the remote system"
-            )
+            
             inventory = self.conn.execute("hostname")
             self.inv["Host Name"] = inventory[0]
-            logger.info("Runnning command 'lsb_release -d' in the remote system")
+            
             inventory = self.conn.execute("lsb_release -d")
             self.inv["Operating System"] = (
                 inventory[0].split("Description:", 1)[1].strip()
             )
-            logger.info("Runnning command 'uname -r' in the remote system")
+            
             inventory = self.conn.execute("uname -r")
             self.inv["Kernel"] = inventory[0]
             return True
