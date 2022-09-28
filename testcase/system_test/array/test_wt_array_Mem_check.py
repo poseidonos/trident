15cdf1ffd84691617aff4e5f73c18a3ad1304555
arun.pillai <arun.pillai@samsung.com>
2022-09-28 13:01:44 +0530

    modified TCs as per SAM score update

--- testcase/system_test/array/test_wt_array_Mem_check.py
+++ testcase/system_test/array/test_wt_array_Mem_check.py
@@ -1,3 +1,4 @@
+from common_libs import *
 import pytest
 
 import logger
@@ -19,61 +20,23 @@ array_list = [
 @pytest.mark.regression
 @pytest.mark.parametrize("raid_type, nr_data_drives, IO", array_list)
 def test_wt_array_Mem_check(
-    setup_cleanup_array_function, raid_type, nr_data_drives, IO
+    array_fixture, raid_type, nr_data_drives, IO
 ):
     logger.info(
         " ==================== Test : test_wt_array_Mem_check ================== "
     )
     try:
-        pos = setup_cleanup_array_function
-        if pos.target_utils.helper.check_pos_exit() == True:
-            assert pos.target_utils.pos_bring_up(data_dict=pos.data_dict) == True
-        assert pos.cli.reset_devel()[0] == True
-
-        assert pos.cli.scan_device()[0] == True
-        assert pos.cli.list_device()[0] == True
-        system_disks = pos.cli.system_disks
-        if len(system_disks) < (nr_data_drives + 1):
-            pytest.skip(
-                f"Insufficient disk count {system_disks}. Required minimum {nr_data_drives + 1}"
-            )
-        data_disk_list = [system_disks.pop(0) for i in range(nr_data_drives)]
-
-        array_name = "posarray1"
-        assert (
-            pos.cli.create_array(
-                write_buffer="uram0",
-                data=data_disk_list,
-                spare=None,
-                raid_type=raid_type,
-                array_name=array_name,
-            )[0]
-            == True
-        )
-
-        assert pos.cli.mount_array(array_name=array_name, write_back=False)[0] == True
-        assert (
-            pos.cli.create_volume("pos_vol_1", array_name=array_name, size="2000gb")[0]
-            == True
-        )
-        assert pos.target_utils.get_subsystems_list() == True
-        assert pos.cli.list_volume(array_name=array_name)[0] == True
-        ss_list = [ss for ss in pos.target_utils.ss_temp_list if "subsystem1" in ss]
-        assert (
-            pos.target_utils.mount_volume_multiple(
-                array_name=array_name, volume_list=pos.cli.vols, nqn_list=ss_list
-            )
-            == True
-        )
-
-        for ss in pos.target_utils.ss_temp_list:
-            assert (
-                pos.client.nvme_connect(ss, pos.target_utils.helper.ip_addr[0], "1158")
-                == True
-            )
-        assert pos.client.nvme_list() == True
-        pos.client.check_system_memory()
-
+        pos = array_fixture
+        pos.data_dict["array"]["pos_array"][0]["raid_type"] = raid_type
+        pos.data_dict["array"]["pos_array"][1]["raid_type"] = raid_type
+       
+        pos.data_dict["array"]["pos_array"][0]["data_device"] = nr_data_drives
+        pos.data_dict["array"]["pos_array"][1]["data_device"] = nr_data_drives
+       
+        assert pos.target_utils.bringupArray(data_dict=pos.data_dict) == True
+        assert pos.target_utils.bringupVolume(data_dict=pos.data_dict) == True
+        run_io(pos)
+        
         if IO == "File":
             dev = [pos.client.nvme_list_out[0]]
 
@@ -81,7 +44,7 @@ def test_wt_array_Mem_check(
             status, mount_point = pos.client.mount_FS(dev)
             assert status == True
 
-            fio_cmd = "fio --name=Rand_RW  --runtime=300 --ramp_time=60  --ioengine=sync  --iodepth=32 --rw=write --size=1000g bs=32kb --direct=1 --verify=md5"
+            fio_cmd = "fio --name=Rand_RW  --runtime=30 --ioengine=sync  --iodepth=32 --rw=write --size=1g bs=32kb --direct=1 --verify=md5"
             assert (
                 pos.client.fio_generic_runner(
                     mount_point, fio_user_data=fio_cmd, IO_mode=False, run_async=True
@@ -114,3 +77,4 @@ def test_wt_array_Mem_check(
             assert pos.client.delete_FS(mount_point) == True
         logger.error(f"Test script failed due to {e}")
         pos.exit_handler(expected=False)
+        assert 0
