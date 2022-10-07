54a54f74be87c5ec68a6a6a2b39c514472367262
Nehal Ram <nehal.ram@samsung.com>
2022-10-07 21:18:25 +0530

    Fixed Raid6 Fault Tolerance Tests

--- testcase/system_test/array_raid6/test_raid6_array_fault_tolerance.py
+++ testcase/system_test/array_raid6/test_raid6_array_fault_tolerance.py
@@ -69,8 +69,9 @@ def test_raid6_array_disk_fail(setup_cleanup_array_function, array_mount):
 @pytest.mark.parametrize("write_mix_read", [80, 20, 50])
 def test_raid6_array_disk_fail_random_io(setup_cleanup_array_function, write_mix_read):
     """
-    The purpose of this test is to create a RAID6 array Mounted as WT or WB. Create 16 volumes and
-    run Block IO Mix of Random Write and Read IO. Fail data disks in different rebuild interval.
+    The purpose of this test is to create two RAID6 arrays with 4 data and 2 spare drives Mounted as WT or WB.
+    Create 16 volumes and run Block IO Mix of Random Write and Read IO. Fail data disks in different rebuild 
+    interval.
     Verification: POS CLI, Fault Tolarance, End to End Data Flow
     """
     logger.info(
@@ -85,7 +86,7 @@ def test_raid6_array_disk_fail_random_io(setup_cleanup_array_function, write_mix
             pytest.skip("Less number of data disk")
 
         assert multi_array_data_setup(pos.data_dict, 2, ("RAID6", "RAID6"), 
-                                      arrays_num_disks, (0, 0), ("WT", "WB"),
+                                      arrays_num_disks, (2, 2), ("WT", "WB"),
                                       (False, False)) == True
         assert pos.target_utils.pos_bring_up(data_dict=pos.data_dict) == True
 
@@ -104,7 +105,7 @@ def test_raid6_array_disk_fail_random_io(setup_cleanup_array_function, write_mix
 
         # Run FIO for 30 minutes
         fio_cmd = f"fio --name=rand_write --ioengine=libaio --rw=readwrite --iodepth=64 --bs=128k "\
-                  "--size=50gb –numjobs=4 --group_reporting --runtime=1800 --time_based --rwmixread={write_mix_read}"
+                  f"--size=50gb --numjobs=4 --group_reporting --runtime=1800 --time_based --rwmixread={write_mix_read}"
 
         # Restart The IO
         out, async_io = pos.client.fio_generic_runner(nvme_devs,
@@ -221,11 +222,11 @@ def test_raid6_array_three_disk_fail_during_io(setup_cleanup_array_function):
         num_vols = 16
         arrays_num_disks = (RAID6_MIN_DISKS, RAID6_MIN_DISKS)
         assert pos.cli.list_device()[0] == True
-        if len(pos.cli.system_disks) < sum(arrays_num_disks):
+        if len(pos.cli.system_disks) < sum(arrays_num_disks) + 2:
             pytest.skip("Less number of data disk")
 
         assert multi_array_data_setup(pos.data_dict, 2, ("RAID6", "RAID6"), 
-                                      arrays_num_disks, (0, 0), ("WT", "WB"),
+                                      arrays_num_disks, (1, 1), ("WT", "WB"),
                                       (False, False)) == True
         assert pos.target_utils.pos_bring_up(data_dict=pos.data_dict) == True
 
@@ -243,12 +244,12 @@ def test_raid6_array_three_disk_fail_during_io(setup_cleanup_array_function):
         for pos_array in pos.data_dict["array"]["pos_array"]:
             array_name = pos_array["array_name"]
 
-            assert pos.client.nvme_list(str=array_name) == True
+            assert pos.client.nvme_list(model_name=array_name) == True
             nvme_devs = pos.client.nvme_list_out
 
             # Run FIO for 30 minutes
-            fio_cmd = f"fio --name=rand_write --ioengine=libaio --rw=readwrite --iodepth=64 --bs=128k "\
-                    "--size=50gb –numjobs=4 --group_reporting --runtime=1800 --time_based --rwmixread={write_mix_read}"
+            fio_cmd = "fio --name=rand_write --ioengine=libaio --rw=readwrite --iodepth=64 --bs=128k "\
+                      "--size=50gb --numjobs=4 --group_reporting --runtime=1800 --time_based"
 
             # Restart The IO
             out, async_io = pos.client.fio_generic_runner(nvme_devs,
@@ -259,15 +260,16 @@ def test_raid6_array_three_disk_fail_during_io(setup_cleanup_array_function):
 
         # Fail three data Disks from Array 1
         logger.info("Wait for 5 minutes before going to disk hot remove")
-        time.sleep(500)
+        time.sleep(300)
 
         array_name = pos.data_dict["array"]["pos_array"][0]["array_name"]
 
         disk_remove_interval_list = [(50, 50, 50)]
         assert array_disks_hot_remove(pos, array_name, disk_remove_interval_list) == True
 
+        assert pos.cli.list_array()[0] == True
         # Wait for async fio to complete
-        assert wait_sync_fio([], nvme_devs, None, async_io[1], sleep_time=120) == True
+        assert wait_sync_fio([], nvme_devs, None, async_io_list[1], sleep_time=120) == True
 
         logger.info(
             " ============================= Test ENDs ======================================"
