55eb9222efde60c07cd6ee81b551106a5906bc8a
Nehal Ram <nehal.ram@samsung.com>
2022-10-10 14:41:48 +0530

    Fxied SAM issue for negative volumes test

--- testcase/system_test/array/test_negative_volume0.py
+++ testcase/system_test/array/test_negative_volume0.py
@@ -2,7 +2,34 @@ import pytest
 
 import logger
 
+from pos import POS
 logger = logger.get_logger(__name__)
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_module():
+
+    global pos, data_dict , array_name
+    pos = POS("pos_config.json")
+    data_dict = pos.data_dict
+    data_dict['array']['num_array'] = 1
+    data_dict['volume']['phase'] = "false"
+    array_name = data_dict["array"]["pos_array"][0]["array_name"]
+    assert pos.target_utils.pos_bring_up(data_dict=data_dict) == True
+    yield pos
+
+def teardown_function():
+    logger.info("========== TEAR DOWN AFTER TEST =========")
+
+    assert pos.cli.list_volume(array_name=array_name)[0] == True
+    for vol_name in pos.cli.vols:
+        assert pos.cli.delete_volume(vol_name, array_name)[0] == True
+            
+    logger.info("==========================================")
+
+
+def teardown_module():
+    logger.info("========= TEAR DOWN AFTER SESSION ========")
+    pos.exit_handler(expected=True)
 
 
 @pytest.mark.regression
@@ -18,14 +45,12 @@ logger = logger.get_logger(__name__)
         ("pos_vol", True),
     ],
 )
-def test_rename_vol_special_char(
-    setup_cleanup_array_function, new_name, expected_result
+def test_rename_vol_special_char(new_name, expected_result
 ):
     logger.info(
         " ==================== Test : test_rename_vol_special_chars ================== "
     )
     try:
-        pos = setup_cleanup_array_function
         array_name = pos.data_dict["array"]["pos_array"][0]["array_name"]
         assert (
             pos.cli.create_volume(array_name=array_name, size="10gb", volumename="vol")[
@@ -50,12 +75,11 @@ def test_rename_vol_special_char(
 
 
 @pytest.mark.regression
-def test_rename_non_exist_vol(setup_cleanup_array_function):
+def test_rename_non_exist_vol():
     logger.info(
         " ==================== Test : test_rename_vol_doub_char ================== "
     )
     try:
-        pos = setup_cleanup_array_function
         array_name = pos.data_dict["array"]["pos_array"][0]["array_name"]
         assert (
             pos.cli.create_volume(array_name=array_name, size="10gb", volumename="vol")[
@@ -80,10 +104,9 @@ def test_rename_non_exist_vol(setup_cleanup_array_function):
 
 
 @pytest.mark.regression
-def test_vol_mnt_unmnt(setup_cleanup_array_function):
+def test_vol_mnt_unmnt():
     logger.info(" ==================== Test : test_vol_mnt_unmnt ================== ")
     try:
-        pos = setup_cleanup_array_function
         array_name = pos.data_dict["array"]["pos_array"][0]["array_name"]
         assert (
             pos.cli.create_volume(array_name=array_name, size="10gb", volumename="vol")[
@@ -109,12 +132,11 @@ def test_vol_mnt_unmnt(setup_cleanup_array_function):
 
 
 @pytest.mark.regression
-def test_rename_vol_after_array_unmnt_mnt(setup_cleanup_array_function):
+def test_rename_vol_after_array_unmnt_mnt():
     logger.info(
         " ==================== Test : test_rename_vol_doub_char ================== "
     )
     try:
-        pos = setup_cleanup_array_function
         array_name = pos.data_dict["array"]["pos_array"][0]["array_name"]
         assert (
             pos.cli.create_volume(array_name=array_name, size="10gb", volumename="vol")[
@@ -146,12 +168,11 @@ def test_rename_vol_after_array_unmnt_mnt(setup_cleanup_array_function):
 
 
 @pytest.mark.regression
-def test_create_vol_larger_array_size(setup_cleanup_array_function):
+def test_create_vol_larger_array_size():
     logger.info(
         " ==================== Test : test_rename_vol_doub_char ================== "
     )
     try:
-        pos = setup_cleanup_array_function
         array_name = pos.data_dict["array"]["pos_array"][0]["array_name"]
         assert pos.cli.info_array(array_name)[0] == True
         array_status = pos.cli.array_info[array_name]
@@ -177,12 +198,11 @@ def test_create_vol_larger_array_size(setup_cleanup_array_function):
 
 
 @pytest.mark.regression
-def test_vol_array_normal_states(setup_cleanup_array_function):
+def test_vol_array_normal_states():
     logger.info(
         " ==================== Test : test_mnt_vol_fault_arrray_state ================== "
     )
     try:
-        pos = setup_cleanup_array_function
         array_name = pos.data_dict["array"]["pos_array"][0]["array_name"]
         assert pos.cli.info_array(array_name)[0] == True
         array_status = pos.cli.array_info[array_name]
