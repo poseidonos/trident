f2f819ec19d97c63d8ef8f20b3eaf88e8a901273
Nehal Ram <nehal.ram@samsung.com>
2022-12-09 10:35:28 +0900

    Removed Debug Message

--- lib/pos.py
+++ lib/pos.py
@@ -64,7 +64,6 @@ class POS:
             data_dict=self.data_dict,pos_as_service= self.pos_AsService
             
         )
-        logger.info("am here")
         self.target_utils = TargetUtils(
             self.target_ssh_obj,
             self.data_dict,
