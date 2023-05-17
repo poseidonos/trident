4ca4a53a3dfb5de2a335a0f84f65ae5a5086dfe6
Nehal Ram <nehal.ram@samsung.com>
2023-05-17 16:25:22 +0530

    Removed regression test and other files

--- testcase/test_bamboo.py
+++
@@ -1,8 +0,0 @@
-import pytest, json, sys, time, random, os
-sys.path.insert(0, '../')
-import logger
-logger = logger.get_logger(__name__)
-
-@pytest.mark.bamboo
-def test_bamboo():
-    logger.info("sample TCS for bamboo")
